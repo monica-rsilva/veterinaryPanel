be1cf738a51744594a56e09a3e620c2c6865ba4d
Monica <monica.ribeiro.silva.2805@gmail.com>
2025-11-30 13:18:05 -0300

    ajustes a serem feitos

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ let initialPreferredPassword = 1;
 let commomPasswords = [];
 let preferredPasswords = [];
 let allPasswords = [];
+let previousPasswords = [];
 
 //função criada para gerar as senhas
 function generatePasswords(passwordType){
@@ -49,7 +50,8 @@ function generatePasswords(passwordType){
     } else{
         commomPasswords.push(password);
     }
-    allPasswords.push(password);
+
+    allPasswords.unshift(password);
     passwordList();
     
     return
@@ -70,13 +72,104 @@ function passwordList(){
 
     }
     skeletonDisabled();
-    displayOnly();
+    displayOnlyGenerated();
+}
+
+//função para chamar próxima senha
+function nextPassword(){
+    
+    let current_password = null;
+
+    if(preferredPasswords.length > 0){
+        current_password = preferredPasswords.shift();
+    } 
+    else if (commomPasswords.length > 0 ) {
+        current_password = commomPasswords.shift();
+    } 
+    else {
+        console.log('sem senhas');
+        return
+    }
+    
+    const index = allPasswords.indexOf(current_password);
+    if(index !== -1){
+        allPasswords.splice(index, 1);
+    }
+
+    let number_password = document.getElementById('password_number');
+    
+    number_password.textContent = current_password;
+    
+    const current_Guiche = generateGuiche();
+    
+    let number_guiche = document.getElementById('guiche_number');
+    
+    number_guiche.textContent = current_Guiche;
+
+    previousPasswords.unshift({
+        password: current_password,
+        guiche: current_Guiche,
+    })
+
+    animation();
+    passwordList();
+    listPreviousPasswords();
+}
+
+function generateGuiche(){
+     return Math.floor(Math.random()*4) + 1;
+}
+
+// // função para animar senha
+function animation(){
+    const strong = document.getElementById('password_number');
+
+    const strongTwo = document.getElementById('guiche_number');
+
+    strong.classList.remove("piscar");
+    strongTwo.classList.remove("piscar");
+
+    void strong.offsetWidth;
+    void strongTwo.offsetWidth;
+
+    strong.classList.add("piscar");
+    strongTwo.classList.add("piscar");
+}
+
+//função para exibir as senhas em lista
+function listPreviousPasswords(){
+   let passwords = document.querySelector('#previous_passwords_list');
+
+    passwords.innerHTML = "";
+
+    for (let i = 0; i < previousPasswords.length; i++){
+    const item = previousPasswords[i];
+        const li = document.createElement('li');
+        li.textContent = `${item.password} - Guichê ${item.guiche}`;
+
+        passwords.appendChild(li);
+
+    }
+    skeletonDisabledTwo();
+    displayOnlyPrevious();
     
 }
 
 //função para exibir apenas 4 elementos do vetor
-function displayOnly(){
-    const elements = document.querySelectorAll('li');
+function displayOnlyGenerated(){
+
+    let ul = document.getElementById('generated_passwords');
+    let elements = ul.querySelectorAll('li');
+
+    elements.forEach((four, index) => {
+        four.style.display = index < 4 ? 'flex' : 'none';
+    });
+}
+
+function displayOnlyPrevious(){
+
+    let ul = document.getElementById('previous_passwords_list');
+    let elements = ul.querySelectorAll('li');
 
     elements.forEach((four, index) => {
         four.style.display = index < 4 ? 'flex' : 'none';
@@ -85,13 +178,25 @@ function displayOnly(){
 
 // função para desativar o "esqueleto"
 function skeletonDisabled(){
-    const skeletons = document.querySelectorAll('#skeleton')
+    const skeletons = document.querySelectorAll('#skeleton_1')
 
     skeletons.forEach((skeleton, index) => {
         if(index < allPasswords.length){
             skeleton.style.display = 'none';
         } else{
             skeleton.style.display = 'flex';
+        }
+    });
+}
+
+function skeletonDisabledTwo(){
+    const skeletons = document.querySelectorAll('#skeleton_2')
+
+    skeletons.forEach((skeleton2, index) => {
+        if(index < previousPasswords.length){
+            skeleton2.style.display = 'none';
+        } else{
+            skeleton2.style.display = 'flex';
         }
     });
 }
